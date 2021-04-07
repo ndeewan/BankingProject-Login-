beffef676856192572a29eaca8c5aa246ca91edf
ndeewan <43903797+ndeewan@users.noreply.github.com>
2021-04-07 10:57:44 +0530

    Add files via upload

---
+++ AccountBal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingLib
+{
+    class AccountBal
+    {
+        public long Account_NO { get; set; }
+        public long Account_Balance { get; set; }
+    }
+}
---
+++ Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingLib
+{
+    class Transaction
+    {
+        public long Sender_Acc { get; set; }
+        public long Receiver_Acc { get; set; }
+        public string Rec_IFSC { get; set; }
+        public long Amount { get; set; }
+        public DateTime Transaction_time { get; set; }
+    }
+}
